605fc05c49bf4db0d9911ec114b1d556181489c8
Jay Franco <jprfranco@gmail.com>
2023-06-08 19:20:30 -0400

    Getting started

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { ClerkProvider } from "@clerk/clerk-react";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
